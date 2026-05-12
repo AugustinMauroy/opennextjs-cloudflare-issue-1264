ca9d036335830a983093761630e5b6ded4a36d3f
Augustin Mauroy <97875033+AugustinMauroy@users.noreply.github.com>
2026-05-12 22:29:43 +0200

    use middleware

--- src/middleware.ts
+++ src/middleware.ts
@@ -4,7 +4,7 @@ import { auth } from "#/lib/auth";
 import { headers } from "next/headers";
 import type { NextRequest } from "next/server";
 
-export async function proxy(request: NextRequest) {
+export async function middleware(request: NextRequest) {
 	const pathname = request.nextUrl.pathname;
 	const session = await auth.api.getSession({
 		headers: await headers(),
